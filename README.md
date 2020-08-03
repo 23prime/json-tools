a624e08a69f5bddf1ca4bff86b45f39ee5af428f
23prime <23.prime.37@gmail.com>
2020-08-03 21:02:46 +0900

    Implement select by key

--- README.md
+++ README.md
@@ -12,9 +12,18 @@ bundle install
 
 ### Select by key and value
 
-1.  Put input file somewhere.
+1.  Put input JSON file somewhere.
 2.  Run following command.
 
 ```sh
 bundle exec ruby src/select_by_key_value.rb {input file} {output file} {key} {value}
+```
+
+### Select by keys
+
+1.  Put input JSON file somewhere.
+2.  Run following command.
+
+```sh
+bundle exec ruby src/select_by_key_value.rb {input file} {output file} {key1} {key2} ...
 ```
--- src/select_by_key.rb
+++ src/select_by_key.rb
@@ -1,7 +1,7 @@
 require 'json'
 
-unless ARGV.size == 4
-  puts 'Error: Set just 4 args -> input file, output file, key, name'
+if ARGV.size < 4
+  puts 'Error: Set over 3 args -> input file, output file, key1, key2, ...'
   exit!
 end
 
@@ -11,7 +11,7 @@ File.open(ARGV[0]) do |file|
   hashmap_array = JSON.load(file)
 end
 
-hashmap_array.select! { |hashmap| hashmap[ARGV[2]] == ARGV[3] }
+hashmap_array.map! { |hashmap| hashmap.select { |key, _| ARGV[2..].include?(key) } }
 
 File.open(ARGV[1], 'w') do |file|
   file.puts(JSON.pretty_generate(hashmap_array))
